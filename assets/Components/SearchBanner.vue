7086962c5097d8f2d213a8d49e0d6705d0d84d63
Kaporal-69 <wis-jer@hotmail.fr>
2021-03-02 10:22:38 +0100

    Ajout de la page affichage resto+producteur

---
+++ assets/Components/SearchBanner.vue
@@ -0,0 +1,11 @@
+<template>
+    <div style="float:left; margin-top:50px; text-align:center; margin-left:15px;">
+        <h6>Selectionnez</h6>
+        <button>Restaurant</button>
+        <button>Producteur</button>
+    </div>
+</template>
+
+<script>
+
+</script>
---
+++ assets/Components/SearchProducteur.vue
@@ -0,0 +1,52 @@
+<template>
+<div id="producteur" style="float:right;">
+    <h3>Liste de Producteur</h3>
+    <div class="producteur-container">
+        <div class="card" v-for="producteur in producteurs" v-bind:key="producteur.nom">
+            <div class="card-image" >
+                <a href="#">
+                    <img :src="producteur.image" style="width:15%; float: left;">
+                </a> 
+                <div class="card-content" style="line-height:35px; margin-left:15%;">
+                    <h5>{{producteur.nom}}</h5>
+                    <p>{{producteur.adresse}}</p>
+                    <p>{{producteur.description}}</p>
+                </div>
+                
+            </div>
+           
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+import image from '../images/placeholder.png'
+export default {
+        name: "SearchProducteur",
+        data() {
+            return {
+                producteurs: [
+                    {
+                        nom: "Aux ptit gourmand",
+                        adresse: "rue du bonheur",
+                        description: "ici une description",
+                        image: image
+                    },
+                    {
+                        nom: "Produit de la ferme",
+                        adresse: "3 rue de la jungle",
+                        description: "ici une description",
+                        image: image
+                    },
+                    {
+                        nom: "exotic box",
+                        adresse: "45 avenue papaya",
+                        description: "ici une description",
+                        image: image
+                    }
+                ]
+            }
+        }
+    }
+</script>

d78e9ec60a44ed74405e31fc7af2c4612e883a62
mellamy <melanie.lamy25@gmail.com>
2021-03-04 11:59:15 +0100

    Mise en page formulaire Login

--- assets/Components/LoginForm.vue
+++ assets/Components/LoginForm.vue
@@ -1,21 +1,17 @@
 <template>
-    <form v-on:submit.prevent="handleSubmit">
-        <div v-show="error" class="alert alert-danger">
+    <form v-on:submit.prevent="handleSubmit" class="login-form">
+        <div v-show="error" class="alert alert-danger" style="color:red;">
             {{ error }}
         </div>
         <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
+            <label for="exampleInputEmail1">Adresse Email</label>
             <input type="email" v-model="email" class="form-control" id="exampleInputEmail1"
-                   aria-describedby="emailHelp" placeholder="Enter email">
+                   aria-describedby="emailHelp" placeholder="votre adresse ici">
         </div>
         <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
+            <label for="exampleInputPassword1">Mot de passe</label>
             <input type="password" v-model="password" class="form-control"
-                   id="exampleInputPassword1" placeholder="Password">
-        </div>
-        <div class="form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">I like cheese</label>
+                   id="exampleInputPassword1" placeholder="votre mot de passe">
         </div>
         <button type="submit" class="btn btn-primary" v-bind:class="{ disabled: isLoading }">Log in</button>
     </form>

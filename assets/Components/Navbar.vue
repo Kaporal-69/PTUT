ede6be54444dc36fb4deafbb964891d198d409e0
mellamy <melanie.lamy25@gmail.com>
2021-03-28 17:25:21 +0200

    Ajout d'un bouton de déconnexion

--- assets/Components/Navbar.vue
+++ assets/Components/Navbar.vue
@@ -6,12 +6,15 @@
         <!-- <li><a href="">Panier</a></li> -->
         <li><a href="/search">Rechercher</a></li>
         <li><a href="/dashboard">Mon Compte</a></li>
-        <li><a href="/login">Se connecter</a></li>
+        <li v-if="!isLoggedIn"><a href="/login">Se connecter</a></li>
+        <li v-else><a v-on:click="logout()" >Se déconnecter</a></li>
+
       </ul>
       <ul id="nav-mobile" class="right hide-on-large-only">
         <li><a href="/search"><i class="material-icons">search</i></a></li>
         <li><a href="/dashboard"><i class="material-icons">account_box</i></a></li>
-        <li><a href="/login"><i class="material-icons">login</i></a></li>
+        <li v-if="!isLoggedIn"><a href="/login"><i class="material-icons">login</i></a></li>
+        <li v-else><a v-on:click="logout()"><i class="material-icons">login</i></a></li>
       </ul>
     </div>
   </nav>
@@ -20,10 +23,24 @@
 <script>
 
   export default {
-    data() {
+    data: function() {
       return {
-        greeting: "Hello world"
+        greeting: "Hello world",
+        isLoggedIn: false
       };
+    },
+    methods: {
+      logout() {
+        localStorage.getItem('user-token') == '';
+        this.isLoggedIn = false;
+      }
+    },
+    beforeMount() {
+        if(localStorage.getItem('user-token') === "undefined") {
+            this.isLoggedIn = false;
+        } else {
+           this.isLoggedIn = true;
+        }
     }
   };
 </script>

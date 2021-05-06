b416e8f24d78eaf88268a3f3db11a3324152f8cf
Stéphane <stephane.baccam@orencash.fr>
2021-05-06 17:44:45 +0200

    Gestion panier

--- assets/Components/DetailRestaurant.vue
+++ assets/Components/DetailRestaurant.vue
@@ -2,17 +2,32 @@
     <div class="detail-restaurant">
         <div class="container">
             <h1>
-                {{this.restaurant.nom}}
+                {{restaurant.nom}}
             </h1>
             <p>
-                {{this.restaurant.description}}
+                {{restaurant.description}}
             </p>
             <ul class="collection with-header">
                 <li class="collection-header"><h2>Plats</h2></li>
                 <div v-for="plat in plats" v-bind:key="plat.id">
-                    <li v-show="plat.restaurateur_id == this.restaurant.id" class="collection-item"><div>{{plat.nom}} - {{plat.prix}}€ <a href="#" class="secondary-content">Commander</a></div></li>
+                    <li v-show="plat.restaurateur_id == restaurant.id" class="collection-item">
+                        <div>{{plat.nom}} - {{plat.prix}}€
+                            <div class="secondary-content">
+                                <button v-on:click="ajoutAuPanier(plat.id)" class="waves-effect waves-light btn">
+                                    <i class="material-icons">add</i>
+                                </button>
+                                {{panier.filter(pan => pan.id == plat.id).length}}
+                                <button v-on:click="supprimerDuPanier(plat.id)" class="waves-effect waves-light btn red">
+                                    <i class="material-icons">remove</i>
+                                </button>
+                            </div>
+                        </div>
+                    </li>
                 </div>
             </ul>
+            <div class="commander">
+                <button class="waves-effect waves-light btn">Commander ({{panier.length}})</button>
+            </div>
         </div>
     </div>
 </template>
@@ -26,6 +41,7 @@ export default {
     props: ['id'],
     data() {
         return {
+            panier: [],
             restaurant: 
                     {
                         id: this.id,
@@ -52,6 +68,24 @@ export default {
                 ]
         }
     },
+    methods: {
+        ajoutAuPanier(id) {
+            var plat = this.plats.find(plat => plat.id == id);
+            console.log(plat);
+            this.panier.push(plat);
+            console.log("Ajout");
+            console.log(this.panier);
+        },
+        supprimerDuPanier(id) {
+            var plat = this.plats.filter(plat => plat.id == id)
+            if(this.panier.includes(plat[0])) {
+                var idx = this.panier.findIndex(p => p.id == id);
+                var removed = this.panier.splice(idx, 1);
+                console.log("Suppression");
+                console.log(this.panier)
+            }
+        }
+    }
     // mounted() {
     //     this.getRestaurateurById()
     // },
